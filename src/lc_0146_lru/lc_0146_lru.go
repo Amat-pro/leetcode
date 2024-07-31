34a2263588c953ebc8b0f69e65353f4b07041be2
luzhongbo <luzb@zenmen.com>
2024-07-31 17:00:39 +0800

    lc_0146_lru

--- src/lc_0146_lru/lc_0146_lru.go
+++ src/lc_0146_lru/lc_0146_lru.go
@@ -1,5 +1,135 @@
 package lru
 
-func lc0146lru() {
+import "fmt"
 
+type LRUCache struct {
+	head *Entry // 虚拟头节点 使用指针，避免构造时发生复制导致地址变化
+	tail *Entry // 虚拟尾节点
+
+	entries map[int]*Entry // map结构
+
+	capacity int
+	len      int
+}
+
+func Constructor(capacity int) LRUCache {
+
+	head := Entry{}
+	tail := Entry{
+		Key: -1,
+	}
+
+	head.Next = &tail
+	tail.Pre = &head
+
+	return LRUCache{
+		head: &head, // 使用指针，避免构造时发生复制导致地址变化
+		tail: &tail,
+
+		entries:  make(map[int]*Entry, capacity),
+		capacity: capacity,
+		len:      0,
+	}
+
+}
+
+func (cache *LRUCache) Get(key int) int {
+	val, ok := cache.entries[key]
+	if !ok {
+		return -1
+	}
+	cache.moveToAt(val, cache.head)
+	return val.Value
+}
+
+func (cache *LRUCache) Put(key, value int) {
+	val, ok := cache.entries[key]
+	if ok {
+		val.Value = value
+		cache.moveToAt(val, cache.head)
+		return
+	}
+
+	if cache.len > 0 && cache.len == cache.capacity {
+		cache.remove(cache.tail.Pre)
+	}
+
+	entry := Entry{
+		Key:   key,
+		Value: value,
+	}
+
+	cache.insertToFront(&entry)
+}
+
+func (cache *LRUCache) Print() {
+
+	node := cache.head.Next
+
+	for node != nil {
+
+		if node == cache.tail {
+			return
+		}
+
+		fmt.Printf("node, key: %d, value: %d\n", node.Key, node.Value)
+		node = node.Next
+	}
+
+}
+
+// move move entry to at's next position
+func (cache *LRUCache) moveToAt(entry, at *Entry) {
+
+	if entry == at {
+		return
+	}
+
+	entry.Pre.Next = entry.Next
+	entry.Next.Pre = entry.Pre
+
+	entry.Pre = at
+	entry.Next = at.Next
+
+	// at.Next = entry
+	entry.Pre.Next = entry
+	entry.Next.Pre = entry
+
+}
+
+// remove rmove entry from cache
+func (cache *LRUCache) remove(entry *Entry) {
+	pre := entry.Pre
+	next := entry.Next
+
+	pre.Next = next
+	next.Pre = pre
+	entry.Pre = nil
+	entry.Next = nil
+
+	delete(cache.entries, entry.Key)
+	cache.len--
+
+}
+
+// insertFront insert entry at front
+func (cache *LRUCache) insertToFront(entry *Entry) {
+	head := cache.head
+	next := head.Next
+
+	head.Next = entry
+	next.Pre = entry
+	entry.Pre = head
+	entry.Next = next
+
+	cache.entries[entry.Key] = entry
+	cache.len++
+
+}
+
+// Entry 双向链表
+type Entry struct {
+	Key, Value int
+
+	Pre, Next *Entry
 }
--- src/lc_0146_lru/lc_0146_lru_test.go
+++ src/lc_0146_lru/lc_0146_lru_test.go
@@ -1,7 +1,51 @@
 package lru
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestLc0146Lru(t *testing.T) {
-	lc0146lru()
+	c1 := Constructor(1)
+	fmt.Println("=============================")
+	c1.Print()
+	c1.Put(1, 1)
+	fmt.Println("=============================")
+	c1.Print()
+	c1.Put(2, 2)
+	fmt.Println("=============================")
+	c1.Print()
+}
+
+func TestLc0146Lru_02(t *testing.T) {
+	c1 := Constructor(2)
+	fmt.Println("init =============================")
+	c1.Print()
+	c1.Put(1, 1)
+	fmt.Println("after put 1 =============================")
+	c1.Print()
+	c1.Put(2, 2)
+	fmt.Println("after put 2 =============================")
+	c1.Print()
+	fmt.Println("get 1: ", c1.Get(1))
+	fmt.Println("after get 1 =============================")
+	c1.Print()
+	c1.Put(3, 3)
+	fmt.Println("after put 3 =============================")
+	c1.Print()
+	fmt.Println("get 2: ", c1.Get(2))
+	fmt.Println("after get 2 =============================")
+	c1.Print()
+	c1.Put(4, 4)
+	fmt.Println("after put 4 =============================")
+	c1.Print()
+	fmt.Println("get 1: ", c1.Get(1))
+	fmt.Println("after get 1 =============================")
+	c1.Print()
+	fmt.Println("get 3: ", c1.Get(3))
+	fmt.Println("after get 3 =============================")
+	c1.Print()
+	fmt.Println("get 4: ", c1.Get(4))
+	fmt.Println("after get 4 =============================")
+	c1.Print()
 }
